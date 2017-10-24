59391fdf91ffc0d93f92972b29ad81ba45f70e20
Bharghava Varun <abvarun226@gmail.com>
2017-10-24 21:13:13 +0530

    Day 1 - Insertion Sort

---
+++ day1/InsertionSort.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func sort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		j := i
+		for j > 0 && arr[j] < arr[j-1] {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+			j--
+		}
+	}
+}
+
+func main() {
+	arr := []int{2, 5, 1, 100, 10, 4, 21, 54}
+
+	fmt.Println("Before Sorting:")
+	for _, x := range arr {
+		fmt.Print(x, " ")
+	}
+	fmt.Println()
+
+	sort(arr)
+
+	fmt.Println("\nAfter Sorting:")
+	for _, x := range arr {
+		fmt.Print(x, " ")
+	}
+	fmt.Println()
+}
