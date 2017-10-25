cfc22e92c534d283d60501f7d2e8e513e0804ceb
Bharghava Varun <abvarun226@gmail.com>
2017-10-26 02:33:38 +0530

    Day2 - Linear Search

---
+++ day2/LinearSearch.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func search(arr []int, key int) int {
+	for idx, val := range arr {
+		if val == key {
+			return idx
+		}
+	}
+	return -1
+}
+
+func main() {
+	arr := []int{1, 8, 4, 5, 100, 123, 564, 32, 10}
+	// key := 6
+	key := 32
+
+	if search(arr, key) == -1 {
+		fmt.Println("Key not found!")
+	} else {
+		fmt.Println("Key found in array")
+	}
+}
