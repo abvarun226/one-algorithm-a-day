641e860b25072ee17e917a3ba8d0bcb6c91797b8
Bharghava Varun <abvarun226@gmail.com>
2017-10-26 18:10:56 +0530

    Day3 - Bubble Sort

---
+++ day3/BubbleSort.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/*
+   Time Complexity: O(n^2)
+   Stable
+   In-place
+*/
+
+func printArray(arr []int) {
+	for _, val := range arr {
+		fmt.Print(val, " ")
+	}
+	fmt.Println()
+}
+
+func sort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 1; j < len(arr); j++ {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+				printArray(arr)
+			}
+		}
+	}
+}
+
+func main() {
+	arr := []int{8, 10, 4, 2, 100, 120, 35, 350, 22, 1, 10}
+
+	fmt.Println("Before Sorting:")
+	printArray(arr)
+
+	fmt.Println("\nDuring Sorting:")
+	sort(arr)
+
+	fmt.Println("\nAfter Sorting:")
+	printArray(arr)
+}
